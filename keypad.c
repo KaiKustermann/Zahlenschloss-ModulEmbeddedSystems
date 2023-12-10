9b3cd1830def13a60b744cc6fcfd083ce200d3ea
KaiKustermann <kai.kuster@gmx.de>
2023-12-10 14:03:54 +0100

    added keypad dirver

--- keypad.c
+++ keypad.c
@@ -3,14 +3,20 @@
 
 #define DDR_KEYPAD DDRD
 #define PORT_KEYPAD PORTD
+#define PIN_KEYPAD PIND
 #define ROW_0 0
 #define ROW_1 1
 #define ROW_2 2
 #define ROW_3 3
-#define COL_0 0
-#define COL_1 1
-#define COL_2 2
-#define COL_3 3
+#define COL_0 4
+#define COL_1 5
+#define COL_2 6
+#define COL_3 7
+
+unsigned char keypad[4][4] = {  {'1','2','3','A'},
+                                {'4','5','6','B'},
+                                {'7','8','9','C'},
+                                {'*','0','#','D'}   };
 
 // using column scanning technique (columns as input to the microcontroller); 
 // detailed: going through the rows (outputs to microcontroller) and applying ground (0) to each row. Then reading the columns (inputs to microcontroller). If the column is 0, the button in this row for this column was pressed.
@@ -22,9 +28,44 @@ keypadInit(){
     // set internal pullup for columns (set to 1) by setting the right ports to 1
     PORT_KEYPAD |= (1U << COL_0) | (1U << COL_1) | (1U << COL_2) | (1U << COL_3);
     // set rows as high (set to 1) by setting the right ports to 1 
-    PORT_KEYPAD &= ~((1 << ROW_0) | (1 << ROW_1) | (1 << ROW_2) | (1 << ROW_3));
+    PORT_KEYPAD  |= (1 << ROW_0) | (1 << ROW_1) | (1 << ROW_2) | (1 << ROW_3);
 }   
 
-findPressedKey(){
-
+char findPressedKey(){
+    uint8_t row=0, input;
+    for(row=0; row<4; row++)
+	{
+        // set all row high
+        PORT_KEYPAD  |= (1 << ROW_0) | (1 << ROW_1) | (1 << ROW_2) | (1 << ROW_3);
+        // set current row to low (0)
+        switch(row)
+            {
+            case 0: PORT_KEYPAD &=~(1<<ROW_0);
+                    break;
+            case 1: PORT_KEYPAD &=~(1<<ROW_1);
+                    break;
+            case 2: PORT_KEYPAD &=~(1<<ROW_2);
+                    break;
+            case 3: PORT_KEYPAD &=~(1<<ROW_3);
+                    break;
+            }
+        // check which column is 0, therefore pressed
+        if(PIN_KEYPAD & (1<<COL_0) == 0){
+            const col = 0;
+            return keypad[row][col];
+        };
+        if(PIN_KEYPAD & (1<<COL_1) == 0){
+            const col = 1;
+            return keypad[row][col];
+        };
+        if(PIN_KEYPAD & (1<<COL_2) == 0){
+            const col = 2;
+            return keypad[row][col];
+        };
+        if(PIN_KEYPAD & (1<<COL_3) == 0){
+            const col = 3;
+            return keypad[row][col];
+        };
+        return 0;
+    }
 }
