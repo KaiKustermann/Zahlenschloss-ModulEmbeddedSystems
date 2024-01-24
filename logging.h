a1959d349130a149f962b866d597649c77f91ff0
KaiKustermann <kai.kuster@gmx.de>
2024-01-24 15:28:16 +0100

    deleted enums.h, put LogLevel in logging.h

--- logging.h
+++ logging.h
@@ -1,15 +1,23 @@
-#include "enums.h"
 #include <avr/io.h>
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
+
+#ifndef LOGGING_H
+#define LOGGING_H
+    typedef enum {
+            INFO,
+            WARNING,
+            ERROR
+        } LogLevel;
     void loggerInit();
     void logMessage(char* message, LogLevel level);
     void logMessageInt(uint8_t message, LogLevel level);
     void logMessageChar(unsigned char message, LogLevel level);
     void logMessageUInt32(uint32_t message, LogLevel level);
+#endif 
 
 #ifdef __cplusplus
 }
